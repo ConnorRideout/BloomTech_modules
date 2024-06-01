a623896f195b6ee39ab1c6e54ba7283a828d44dc
Connor Rideout <connorrideout13@gmail.com>
2024-05-31 20:29:42 -0400

    completed Sprint 7 Module 1 (basic only)

---
+++ Sprint7/S7M1-project-react-router/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import axios from 'axios'
+
+import SavedList from './Movies/SavedList'
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+
+export default function App() {
+    const [saved, setSaved] = useState([]) // Stretch: the ids of "saved" movies
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const getMovies = () => {
+            axios
+                .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
+                .then(response => {
+                    // Study this response with a breakpoint or log statements
+                    // and set the response data as the 'movies' slice of state
+                    // console.log(response.data)
+                    setMovies(response.data)
+                })
+                .catch(error => {
+                    console.error('Server Error', error)
+                })
+        }
+        getMovies()
+    }, [])
+
+    const addToSavedList = id => {
+        // This is stretch. Prevent the same movie from being "saved" more than once
+    }
+
+    return (
+        <div>
+            <SavedList list={[ /* This is stretch */]} />
+
+            {/* <div>Replace this Div with your Routes</div> */}
+            <Routes>
+                <Route path="/" element={<MovieList movies={movies} />} />
+                <Route path="movies/:id" element={<Movie />} />
+            </Routes>
+        </div>
+    )
+}
---
+++ Sprint7/S7M1-project-react-router/src/Movies/MovieList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function MovieList(props) {
+    return (
+        <div className="movie-list">
+            {props.movies.map(movie => (
+                <MovieDetails key={movie.id} movie={movie} />
+            ))}
+        </div>
+    );
+}
+
+function MovieDetails(props) {
+    const { title, director, metascore, id } = props.movie;
+    const navigate = useNavigate()
+
+    return (
+        <div className="movie-card" onClick={() => navigate(`movies/${id}`)}>
+            <h2>{title}</h2>
+            <div className="movie-director">
+                Director: <em>{director}</em>
+            </div>
+            <div className="movie-metascore">
+                Metascore: <strong>{metascore}</strong>
+            </div>
+        </div>
+    );
+}
