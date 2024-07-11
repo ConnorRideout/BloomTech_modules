0db4f244d73b6dfcef3a99ea5c3f2cb301a2e283
Connor Rideout <connorrideout13@gmail.com>
2024-07-11 18:28:53 -0400

    completed Sprint 10 Module 3

--- Sprint10/S10M3-redux-toolkit/frontend/state/quotesSlice.js
+++ Sprint10/S10M3-redux-toolkit/frontend/state/quotesSlice.js
@@ -1,28 +1,72 @@
 // ✨ create your `quotesSlice` in this module
+import { createSlice } from "@reduxjs/toolkit"
 
 let id = 1
 const getNextId = () => id++
 const initialState = {
-  displayAllQuotes: true,
-  highlightedQuote: null,
-  quotes: [
-    {
-      id: getNextId(),
-      quoteText: "Don't cry because it's over, smile because it happened.",
-      authorName: "Dr. Seuss",
-      apocryphal: false,
-    },
-    {
-      id: getNextId(),
-      quoteText: "So many books, so little time.",
-      authorName: "Frank Zappa",
-      apocryphal: false,
-    },
-    {
-      id: getNextId(),
-      quoteText: "Be yourself; everyone else is already taken.",
-      authorName: "Oscar Wilde",
-      apocryphal: false,
-    },
-  ],
+    displayAllQuotes: true,
+    highlightedQuote: null,
+    quotes: [
+        {
+            id: getNextId(),
+            quoteText: "Don't cry because it's over, smile because it happened.",
+            authorName: "Dr. Seuss",
+            apocryphal: false,
+        },
+        {
+            id: getNextId(),
+            quoteText: "So many books, so little time.",
+            authorName: "Frank Zappa",
+            apocryphal: false,
+        },
+        {
+            id: getNextId(),
+            quoteText: "Be yourself; everyone else is already taken.",
+            authorName: "Oscar Wilde",
+            apocryphal: false,
+        },
+    ],
 }
+
+const slice = createSlice({
+    name: 'quotes_state',
+    initialState,
+    reducers: {
+        deleteQuote: (state, { payload }) => {
+            state.quotes = state.quotes.filter(qt => qt.id != payload)
+        },
+        setHighlightedQuote: (state, { payload }) => {
+            state.highlightedQuote = state.highlightedQuote == payload ? null : payload
+        },
+        editQuoteAuthenticity: (state, { payload }) => {
+            const quote = state.quotes.find(qt => qt.id == payload)
+            quote.apocryphal = !quote.apocryphal
+        },
+        toggleVisibility: state => {
+            state.displayAllQuotes = !state.displayAllQuotes
+        },
+        createQuote: {
+            prepare({ authorName, quoteText }) {
+                const newQuote = {
+                    id: getNextId(),
+                    quoteText,
+                    authorName,
+                    apocryphal: false
+                }
+                return { payload: newQuote }
+            },
+            reducer(state, { payload }) {
+                state.quotes.push(payload)
+            }
+        }
+    }
+})
+
+export default slice.reducer
+export const {
+    deleteQuote,
+    setHighlightedQuote,
+    editQuoteAuthenticity,
+    toggleVisibility,
+    createQuote
+} = slice.actions
