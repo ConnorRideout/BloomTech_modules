af346bc6880af766b5ba17743081c9038de4b6aa
Connor Rideout <connorrideout13@gmail.com>
2024-07-22 19:18:26 -0400

    completed Sprint 11 Module 2

--- Sprint11/S11M2-project-auth/frontend/components/StarsList.js
+++ Sprint11/S11M2-project-auth/frontend/components/StarsList.js
@@ -1,23 +1,48 @@
 import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import axios from 'axios'
 
 export default function StarsList() {
-  const [stars, setStars] = useState([])
-  return (
-    <div className="container">
-      <h3>StarsList <button>Logout</button></h3>
-      {stars.length > 0 ? (
-        <div>
-          {stars.map((star) => (
-            <div key={star.id} style={{ marginBottom: '20px' }} className="star">
-              <h4>{star.fullName}</h4>
-              <p>Born: {star.born}</p>
-              <p>{star.bio}</p>
-            </div>
-          ))}
+    const [stars, setStars] = useState([])
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (!token) {
+            logout()
+        } else {
+            axios.get('/api/stars', { headers: { Authorization: token } })
+                .then(({ data }) => {
+                    setStars(data)
+                })
+                .catch(err => {
+                    if (err?.response?.status == 401) logout()
+                })
+        }
+    }, [])
+    const navigate = useNavigate()
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        navigate('/')
+    }
+
+    return (
+        <div className="container">
+            <h3>StarsList
+                <button onClick={logout}>Logout</button>
+            </h3>
+            {stars.length > 0 ? (
+                <div>
+                    {stars.map((star) => (
+                        <div key={star.id} style={{ marginBottom: '20px' }} className="star">
+                            <h4>{star.fullName}</h4>
+                            <p>Born: {star.born}</p>
+                            <p>{star.bio}</p>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p>No stars found.</p>
+            )}
         </div>
-      ) : (
-        <p>No stars found.</p>
-      )}
-    </div>
-  )
+    )
 }
